475daeb6205edccb8594136b5a5f1813e792b15d
Antoine <herrentoine21@gmail.com>
2020-10-21 13:28:18 +0200

    Changed engine to postgresql

--- projetIA/settings.py
+++ projetIA/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'projetIA.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                         'NAME': 'projetIA',
+                         'USER': 'postgres',
+                         'PASSWORD': 'projetia',
+                         'HOST': 'localhost',
+                         'PORT': '5432',
+                         }
+             }
 
 
 # Password validation
