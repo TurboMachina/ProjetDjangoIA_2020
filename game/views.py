40186030950212b2a3f060f641fc859a787916e4
anbaert <antoine.baert@hotmail.be>
2020-11-10 15:33:30 +0100

    avancement Game/views et new_game.html

--- game/views.py
+++ game/views.py
@@ -42,26 +42,6 @@ def index(request):
 
         return HttpResponse("KO")
 
-    game_state = {
-        "game_id" : 11,
-        "board" : [[1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,2]],
-        "players" : [{
-                "id" :  10,
-                "name" : "Alice",
-                "color" : "cyan",
-                "position" : [0,0]
-            },{
-                "id" :  20,
-                "name" : "Bob",
-                "color" : "orange",
-                "position" : [7,7]
-            }],
-        "current_player" : 1,
-        "code" : 0
-    }
-
-    return HttpResponse(json.dumps(game_state))
-
 def apply_move(request) :
 
     random_board = [[random.randint(0,2) for i in range(8)]for i in range(8)]
@@ -83,3 +63,80 @@ def apply_move(request) :
         "code" : 0
     }
     return JsonResponse(game_state)
+
+
+
+# 0 = case non prise
+# 1 = case prise par le joueur 1
+# 2 = case prise par le joueur 2
+
+#  x
+# |
+# |
+# |______ y
+
+
+
+
+class UserGame : 
+
+    def __init__(self, color, userNumber, posUserX, posUserY): # Constructeur d'un user
+        self._userNumber = userNumber # savoir si le user est 1 ou 2
+        self._color = color
+        self._posX = posUserX
+        self._posY = posUserY
+        self._case_claimed = 0
+    
+    def play(self):
+        # ask user to click on a button UP or DOWN or LEFT or RIGHT
+        # Recuperer le mouvement
+
+        return movement
+    
+    def move(self, movement):
+        if movement == "UP":
+            x = self._posX
+            y = self._posY - 1
+
+        elif movement == "DOWN":
+            x = self._posX
+            y = self._posY + 1
+
+        elif movement == "LEFT":
+            x = self._posX - 1
+            y = self._posY
+
+        elif movement == "RIGHT":
+            x = self._posX + 1
+            y = self._posY 
+
+        return (x, y) # retourne les coordonées de la nouvelle case 
+    
+
+    def user_take_one_case(self): # mettre dans une boucle avec le nb de case prise par le mouvement
+        self._case_claimed += 1
+    
+
+
+
+class Game : 
+    
+    def __init__(self, id, currentUser, gameState, userGames): # Constructeur d'une game
+        self._id = id
+        self._currentUser = currentUser
+        self._gameState = gameState
+        self._userGames = userGames
+    
+    def _init_board(self):
+        self._gameState = "1" + ("0" * (len(self.game_state) - 2)) + "2" # Mettre 1 en haut à gauche, 0 partout et 2 en bas à droite
+
+    def is_out_of_limits(self, x, y) : 
+        if (x >= 0 and x < len(self.game_state)) and (y >= 0 and y < len(self.game_state)): # ? pas sur du len()
+            return False
+
+        return True
+
+
+
+
+    
