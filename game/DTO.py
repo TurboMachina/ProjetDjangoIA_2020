c0f8d380b82d57db93fd0a27c157b370bb195555
anbaert <antoine.baert@hotmail.be>
2020-11-12 19:01:05 +0100

    Modification DTO et business
    Ajout class user
    Ajout methodes pour trouver winner
    Quelques modifs

--- game/DTO.py
+++ game/DTO.py
@@ -1,4 +1,20 @@
+#_______________________________________________________________________________
+# CLASS USER
+#_______________________________________________________________________________
 
+class User : 
+
+    # Constructeur d'un User
+    def __init__(self, user_id, username, password): 
+        self._id = user_id 
+        self._username = username
+        self._password = password
+
+    # Getters utiles
+
+    def username(self):
+        return self._username
+    
 
 #_______________________________________________________________________________
 # CLASS USERGAME
@@ -7,13 +23,21 @@
 class UserGame : 
 
     # Constructeur d'un UserGame
-    def __init__(self, color, userNumber, posUserX, posUserY): 
+    def __init__(self, user, color, userNumber, posUserX, posUserY): 
+        self._user = user
         self._userNumber = userNumber # savoir si le user est 1 ou 2
         self._color = color
         self._posX = posUserX
         self._posY = posUserY
     
-    # setters et getters ?
+    # Getters utiles
+
+    def user(self):
+        return self._user
+
+    def userNumber(self):
+        return self._userNumber
+
     
 # --------------------- METHODES GENERALEs DU JOUEUR ---------------------
 
@@ -54,13 +78,28 @@ class UserGame :
 class Game : 
     
     # Constructeur d'une game
-    def __init__(self, id, currentUser, gameState, userGames):
+    def __init__(self, id, gameState, userGames):
         self._id = id
-        self._currentUser = currentUser
         self._gameState = gameState
         self._userGames = userGames
+        self._cells_left = 64 # Initialize le nombre de case non prise à 64
+        self._turn = 0 # Initialize le tour à 0
     
-    # setters et getters ?
+
+    # Getters utiles
+
+    def userGames(self):
+        return self._userGames
+    
+    def gameState(self):
+        return self._gameState
+    
+    def cells_left(self):
+        return self._cells_left
+    
+    def turn(self):
+        return self._turn
+    
     
 
 # --------------------- METHODES GENERALEs AU JEU ---------------------
@@ -70,35 +109,47 @@ class Game :
         # Mettre 1 en haut a gauche, 0 partout et 2 en bas a droite
         gameStateInt = "1" + ("0" * (len(self.gameState) - 2)) + "2" 
         self._gameState = [int(x) for x in str(gameStateInt)] 
+        self._cells_left -= 2
 
-    # Passe au joueur suivant
+    # Passe au joueur suivant (UserNumber est soit 1 soit 2)
     def next_turn(turn) :
         if(turn == 1) : 
             return 2
         return 1
 
-    def get_state(self, gameState) : 
-        # Savoir si toutes les cases sont prises, retourner -1 si c'est le cas
-
-    # On change la case
-    def update_cell(self, user, x, y) : 
-        cell = # case x , y
-        if(cell == "0") : 
-            cell = user.userNumber
+    # Savoir si la game est finie
+    def get_state(self) : 
+        if(self._cells_left == 0) : 
+            return False
+        return True
     
-    # Game done ?
-    def get_state(self, gameState) : 
-        # parcourir le tableau et voir si toutes les cases sont prises
+    def get_winner(self, gameState){
+        nbOne = 0
+        nbTwo = 0
+        for(x = 0; x < len(self.gameState); x++) : 
+            for(y = 0; y < len(self.gameState); y++) : 
+                if(self.gameState[x][y] == "1"):
+                    nbOne++
+                else : 
+                    nbTwo++
 
+        if nbOne > nbTwo:
+            return self.userGames[0].user.username
+        return self.userGames[1].user.username
+    }
+    
     def print_results() : 
-        # Afficher gagnant 
+        username = get_winner()
+        # afficher le winner
 
-    def update_board(self, player, movement) : 
-        new_position_xy = player.move(movement)
-        # function qui update le board
-    
 
     # function qui compte et remplit les cases prisent 
+    def update_cells(self) : 
+
+    # function qui update le board
+    def update_board(self, player, movement) : 
+        new_position_xy = player.move(movement)
+        update_cells()
 
 
 
@@ -110,12 +161,12 @@ class Game :
             return False
         return True
     
-    def case_already_taken(movement) : 
+    def ccell_already_taken(movement) : 
         # function qui regarde si il ne va pas sur une case de ladversaire
 
     def movement_ok(movement) : 
         if(!(is_out_of_limits(movement))) : 
-            if(!(case_already_taken(movement))) : 
+            if(!(cell_already_taken(movement))) : 
                 return True
         return False
     
--- game/business.py
+++ game/business.py
@@ -16,18 +16,18 @@ def play(game) :
     # Initiallisation du board
     game.init_board()
 
-    players = game.UserGames # Recuperation des deux joueurs sous forme dun tableau
+    players = game.userGames # Recuperation des deux joueurs sous forme dun tableau
 
-    game.userNumber = random_user_number() # obtenir le numero du joueur qui commence 1 ou 2
+    game.turn = random_user_number() # obtenir le numero du joueur qui commence 1 ou 2
 
-    while(game.get_state() != -1) : # boucle tant que toutes les cases ne sont pas prises
+    while(!(game.get_state())) : # boucle tant que toutes les cases ne sont pas prises
 
         game.print_board()
-        movement = players[game.userNumber].play() # demande de jouer
+        movement = players[game.turn].play() # demande de jouer
 
         if(movement_ok(movement)) : # verification que le movement est possible (reprend plusieurs fonctions)
-            game.update_board(players[game.userNumber], movement) # update le board avec le movement du joueur et les cases prises
-            game.userNumber = game.next_turn(turn) # change de tour
+            game.update_board(players[game.turn], movement) # update le board avec le movement du joueur et les cases prises
+            game.turn = game.nextturn(game.turn) # change de tour
 
         else : 
             print_error() # afficher message erreur car mouvement pas possible
@@ -45,13 +45,13 @@ def play(game) :
 if __name__ == "__main__":
 
     # Information a retouver grace au model BD ? 
-    player1 = UserGame(color, userNumber, posUserX, posUserY)
-    player2 = UserGame(color, userNumber, posUserX, posUserY)
+    player1 = UserGame(user, color, userNumber, posUserX, posUserY)
+    player2 = UserGame(user, color, userNumber, posUserX, posUserY)
 
     players = [player1, player2]
 
     # Information a retouver grace au model BD ? 
-    game = Game(id, currentUser, gameState, players)
+    game = Game(game_id, currentUser, gameState, players)
 
     play(game)
 
