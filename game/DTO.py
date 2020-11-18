64f99f7c4eb9298365c28e0d614a80ba4488d304
anbaert <antoine.baert@hotmail.be>
2020-11-18 17:14:34 +0100

    Modif IA
    userGame -> user

--- game/DTO.py
+++ game/DTO.py
@@ -67,7 +67,7 @@ class UserGame :
             x = self._posX
             y = self._posY + 1
 
-        elif movement == "LEFT":
+        elif movement == "LEFT":L
             x = self._posX - 1
             y = self._posY
 
--- game/IA.py
+++ game/IA.py
@@ -8,10 +8,10 @@ import numpy as np
 from random import randint
 import random
 
-class IA(UserGame) : 
+class IA(User) : 
 
-    def __init__(self, user, color, userNumber, posUserX, posUserY, epsilon=0.90, learning_rate=0.1,) : # IA est un joueur, héritage
-        UserGame.__init__(self, user, color, userNumber, posUserX, posUserY)
+    def __init__(self, user_id, epsilon=0.90, learning_rate=0.1,) : # IA est un joueur, héritage
+        User.__init__(self, user_id)
         self._epsilon = epsilon
         self._learning_rate = learning_rate
         self._qtable = []
@@ -37,7 +37,7 @@ class IA(UserGame) :
     
     # faire un mouvement en fonction de l epsilone greedy (decouverte ou pas)
     # On modifie la function déjà présente dans le DTO pour un user normal
-    def play(state, Q, epsilon):
+    def move(state, Q, epsilon):
     if random.uniform(0, 1) < epsilon:
         action = randint(0, 3) # decouverte, random entre les 4 actions possible
     else: 
@@ -45,7 +45,7 @@ class IA(UserGame) :
     return action
     
 
-    def move() :
+    def play() :
         # state = ? place initiale (TODO)
         # action = play(state, self.Q, self.epsilon)
 
