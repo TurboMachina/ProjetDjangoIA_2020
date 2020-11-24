226f35463aa87d6406cb36bf5229dcd63fce9619
anbaert <antoine.baert@hotmail.be>
2020-11-24 08:40:05 +0100

    Avancement IA

--- game/IA.py
+++ game/IA.py
@@ -2,8 +2,6 @@
 # IA
 #_______________________________________________________________________________
 
-# J'ai créé un fichier à part pour l'IA car je savais pas trop ou la mettre (TODO) 
-
 import numpy as np
 from random import randint
 import random
@@ -36,6 +34,15 @@ class IA(User) :
     @property
     def qtable(self) : 
         return self._qtable
+    
+    @property
+    def posX(self) : 
+        return self.posX
+    
+    @property
+    def posY(self) : 
+        return self.posY
+    
 
 
     
@@ -47,21 +54,22 @@ class IA(User) :
         action = np.argmax(Q[state]) # prendre le meilleur mouvement possible dans la table Q en fonction du state
     return action
 
-    def move(self, action) : 
-        self._posY = self._posY + self.actions[action][0]
-        self._posX = self._posX + self.actions[action][1]
 
-        return (self.y + self.x) , self.qtable[self._posY][self._posX] # ! quand on retourne le state, il doit etre unique pour chaque case (0-64) TODO
+    def move(self, action) : 
+        self.posY = self.posY + self.actions[action][0]
+        self.posX = self.posX + self.actions[action][1]
+
+        return (self.posY, self.posX) , self.qtable[self._posY][self.posX] # retounr le state (unique) le reward associe 
     
 
     def play(game) :
-        # state =  recuperer x - y et les transformer pour qu'ils soit entre 0 et 64 TODO
+        state =  (self.posY, self.posX) # state actuel ?
 
-        action = self.move(state, self.qtable, self.epsilon)
+        action = self.take_action(state, self.qtable, self.epsilon) 
 
         nextState, reward = self.move(action)
 
-        nextAction = self.move(nextState, self.Q, 0.0)
+        nextAction = self.take_action(nextState, self.qtable, 0.0) # La meilleure action
 
         qtable[state][action] = qtable[state][action] + self.learning_rate * (reward + self.epsilon * qtable[nextState][nextAction] - qtable[state][action])
 
