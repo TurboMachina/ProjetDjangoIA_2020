e345d97a1bd4a1daf839020debe33b088b57f017
Antoine Herrent <herrentoine21@gmail.com>
2020-12-28 16:27:56 +0100

    came_case2

--- game/business.py
+++ game/business.py
@@ -16,58 +16,58 @@ def random_user_number():
     return random.randint(1, 2)
 
 
-def ia_plays(user_game, iaDTO, game, gameDTO):
-    userGame.move()
+def ia_plays(user_game, iaDTO, game, game_dto):
+    user_game.move()
     if game.game_over():
-        gameDTO.winner = gameDTO.get_winner()
-    gameDTO.next_turn()
+        game_dto.winner = game_dto.get_winner()
+    game_dto.next_turn()
 
 
-def maj_pos(newPosX, newPosY, gameDTO):
-    if not gameDTO.movement_ok({"x": newPosX, "y": newPosY}, gameDTO.turn):
+def maj_pos(new_pos_x, new_pos_y, game_dto):
+    if not game_dto.movement_ok({"x": new_pos_x, "y": new_pos_y}, game_dto.turn):
         raise InvalidMoveError()
-    gameDTO.update_board(gameDTO.turn, {"posX": newPosX, "posY": newPosY})
-    if gameDTO.game_over():
-        gameDTO.winner = gameDTO.get_winner()
-    gameDTO.next_turn()
+    game_dto.update_board(game_dto.turn, {"posX": new_pos_x, "posY": new_pos_y})
+    if game_dto.game_over():
+        game_dto.winner = game_dto.get_winner()
+    game_dto.next_turn()
 
 
-def move(userGame, moveX, moveY, gameDTO):
-    newPosX = userGame.posUserX + moveX
-    newPosY = userGame.posUserY + moveY
-    maj_pos(newPosX, newPosY, gameDTO)
-    return (newPosX, newPosY)
+def move(user_game, move_x, move_y, game_dto):
+    new_pos_x = user_game.posUserX + move_x
+    new_pos_y = user_game.posUserY + move_y
+    maj_pos(new_pos_x, new_pos_y, game_dto)
+    return new_pos_x, new_pos_y
 
 
-def save_userGame(userGame, newPosX, newPosY):
-    userGame.posUserX = newPosX
-    userGame.posUserY = newPosY
-    userGame.save()
+def save_user_game(user_game, new_pos_x, new_pos_y):
+    user_game.posUserX = new_pos_x
+    user_game.posUserY = new_pos_y
+    user_game.save()
 
 
-def save_game_state(game, gameDTO):
-    game.gameState = gameDTO.gameState
-    game.currentUser = gameDTO.turn
-    game.winner = gameDTO.winner
+def save_game_state(game, game_dto):
+    game.gameState = game_dto.gameState
+    game.currentUser = game_dto.turn
+    game.winner = game_dto.winner
     game.save()
 
 
-def save_move(userGame, newPosX, newPosY, game, gameDTO):
-    save_userGame(userGame, newPosX, newPosY)
-    save_game_state(game, gameDTO)
+def save_move(user_game, new_pos_x, new_pos_y, game, game_dto):
+    save_user_game(user_game, new_pos_x, new_pos_y)
+    save_game_state(game, game_dto)
 
 
-def assign_pos(userGame, posX, posY=None):
-    if not posY:
-        posY = posX
-    userGame.posUserX = posX
-    userGame.posUserY = posY
-    userGame.save()
+def assign_pos(user_game, pos_x, pos_y=None):
+    if not pos_y:
+        pos_y = pos_x
+    user_game.posUserX = pos_x
+    user_game.posUserY = pos_y
+    user_game.save()
 
 
-def assign_duo(userGame1, userGame2):
-    assign_pos(userGame1, 0)
-    assign_pos(userGame2, 7)
+def assign_duo(user_game1, user_game2):
+    assign_pos(user_game1, 0)
+    assign_pos(user_game2, 7)
 
 
 def start_game(game_id, user):
@@ -85,54 +85,54 @@ def start_game(game_id, user):
 
     save_game_state(game, gameDTO)
 
-    userGames = gmodels.UserGame.objects.filter(game__id=game.id)
+    user_games = gmodels.UserGame.objects.filter(game__id=game.id)
 
-    if userGames[0].userNumber == 1:
-        assign_duo(userGames[0], userGames[1])
-        userGame1, userGame2 = userGames[0], userGames[1]
+    if user_games[0].userNumber == 1:
+        assign_duo(user_games[0], user_games[1])
+        user_game1, user_game2 = user_games[0], user_games[1]
     else:
-        assign_duo(userGames[1], userGames[0])
-        userGame1, userGame2 = userGames[1], userGames[0]
+        assign_duo(user_games[1], user_games[0])
+        user_game1, user_game2 = user_games[1], user_games[0]
 
-    if userGame2.ia and userGame2.userNumber == gameDTO.turn:
-        (moveX, moveY) = play(userGame1.posUserX, userGame1.posUserY, userGame2.posUserX, userGame2.posUserY,
-                              gameDTO.gameState, userGame2,
-                              gameDTO.possible_actions(userGame2.posUserX, userGame2.posUserY, userGame2.userNumber),
+    if user_game2.ia and user_game2.userNumber == gameDTO.turn:
+        (moveX, moveY) = play(user_game1.posUserX, user_game1.posUserY, user_game2.posUserX, user_game2.posUserY,
+                              gameDTO.gameState, user_game2,
+                              gameDTO.possible_actions(user_game2.posUserX, user_game2.posUserY, user_game2.userNumber),
                               gameDTO.turn)
-        (newPosX, newPosY) = move(userGames[1], moveX, moveY, gameDTO)
-        save_move(userGames[1], newPosX, newPosY, game, gameDTO)
+        (newPosX, newPosY) = move(user_games[1], moveX, moveY, gameDTO)
+        save_move(user_games[1], newPosX, newPosY, game, gameDTO)
 
     return game
 
 
 def apply_move(game_id, user, movement):
-    userGame = gmodels.UserGame.objects.filter(game__id=game_id, userId__id=user.id).first()
-    if not userGame:
+    user_game = gmodels.user_game.objects.filter(game__id=game_id, userId__id=user.id).first()
+    if not user_game:
         raise NotPlayerError()
 
-    if not userGame.game.gameState:
+    if not user_game.game.gameState:
         raise GameNotStartedError()
 
-    if not userGame.userNumber == userGame.game.currentUser:
+    if not user_game.userNumber == user_game.game.currentUser:
         raise NotYourTurnError()
 
-    game = userGame.game
-    userGame2 = gmodels.UserGame.objects.filter(game__id=game.id).exclude(userId__id=user.id).first()
+    game = user_game.game
+    user_game2 = gmodels.user_game.objects.filter(game__id=game.id).exclude(userId__id=user.id).first()
     gameDTO = mapGame(game)
     if not gameDTO.winner:
-        (newPosX, newPosY) = move(userGame, movement["x"], movement["y"], gameDTO)
+        (newPosX, newPosY) = move(user_game, movement["x"], movement["y"], gameDTO)
         print("test")
 
-        if userGame2.ia and not gameDTO.game_over():
-            (moveX, moveY) = play(newPosX, newPosY, userGame2.posUserX, userGame2.posUserY, gameDTO.gameState,
-                                  userGame2, gameDTO.possible_actions(userGame2.posUserX, userGame2.posUserY,
-                                                                      userGame2.userNumber), gameDTO.turn)
-            (newPosXAi, newPosYAi) = move(userGame2, moveX, moveY, gameDTO)
-            save_userGame(userGame2, newPosXAi, newPosYAi)
+        if user_game2.ia and not gameDTO.game_over():
+            (moveX, moveY) = play(newPosX, newPosY, user_game2.posUserX, user_game2.posUserY, gameDTO.gameState,
+                                  user_game2, gameDTO.possible_actions(user_game2.posUserX, user_game2.posUserY,
+                                                                      user_game2.userNumber), gameDTO.turn)
+            (newPosXAi, newPosYAi) = move(user_game2, moveX, moveY, gameDTO)
+            save_user_game(user_game2, newPosXAi, newPosYAi)
 
-        save_move(userGame, newPosX, newPosY, game, gameDTO)
+        save_move(user_game, newPosX, newPosY, game, gameDTO)
 
-    gameDTO.players = mapMultipleUsers([userGame, userGame2])
+    gameDTO.players = mapMultipleUsers([user_game, user_game2])
 
     return gameDTO
 
@@ -143,8 +143,8 @@ def resume_game(game_id, user_id):
         raise NotPlayerError()
 
     gameDTO = mapGame(game)
-    userGames = gmodels.UserGame.objects.filter(game__id=game_id).all()
-    gameDTO.players = mapMultipleUsers(userGames)
+    user_games = gmodels.user_game.objects.filter(game__id=game_id).all()
+    gameDTO.players = mapMultipleUsers(user_games)
     return gameDTO
 
 
@@ -164,7 +164,7 @@ def create_game(form, user_id):
     if hex_color == 0:
         raise ForbidenColorError()
 
-    gmodels.UserGame.objects.create(userId=user, game=game, color=hex_color, userNumber=1)
+    gmodels.user_game.objects.create(userId=user, game=game, color=hex_color, userNumber=1)
 
     return game
 
@@ -190,7 +190,7 @@ def verrification_before_join(label, form, game_id):
 
     if hex_color == 0:
         raise ForbidenColorError()
-    if gmodels.UserGame.objects.filter(game__id=game_id, color=hex_color).exists():
+    if gmodels.user_game.objects.filter(game__id=game_id, color=hex_color).exists():
         raise ColorAlreadyTakenError()
 
     return game, hex_color
@@ -201,7 +201,7 @@ def join_ia(game_id, ia_id, form):
 
     ai = AI.objects.filter(id=ia_id).first()
 
-    gmodels.UserGame.objects.create(ia=ai, game=game, color=hex_color, userNumber=2)
+    gmodels.user_game.objects.create(ia=ai, game=game, color=hex_color, userNumber=2)
 
 
 def join_game(game_id, user, form):
@@ -210,7 +210,7 @@ def join_game(game_id, user, form):
     if user in game.players.all():
         raise AlreadyPlayerError()
 
-    gmodels.UserGame.objects.create(userId=user, game=game, color=hex_color, userNumber=2)
+    gmodels.user_game.objects.create(userId=user, game=game, color=hex_color, userNumber=2)
 
 
 def train(ia_id, form):
@@ -222,9 +222,9 @@ def train(ia_id, form):
 
         ia = AI.objects.filter(id=ia_id).first()
 
-        userGame1 = gmodels.UserGame.objects.create(game=game, ia=ia, color=0, userNumber=1)
-        userGame2 = gmodels.UserGame.objects.create(game=game, ia=ia, color=1, userNumber=2)
-        userGames = [userGame1, userGame2]
+        user_game1 = gmodels.user_game.objects.create(game=game, ia=ia, color=0, userNumber=1)
+        user_game2 = gmodels.user_game.objects.create(game=game, ia=ia, color=1, userNumber=2)
+        user_games = [user_game1, user_game2]
 
         gameDTO = mapGame(game)
         gameDTO.init_board()
@@ -232,13 +232,13 @@ def train(ia_id, form):
 
         save_game_state(game, gameDTO)
 
-        assign_duo(userGame1, userGame2)
+        assign_duo(user_game1, user_game2)
 
         while not gameDTO.game_over():
-            current_userGame = userGames[gameDTO.turn - 1]
-            (moveX, moveY) = play(userGame1.posUserX, userGame1.posUserY, userGame2.posUserX, userGame2.posUserY,
-                                  gameDTO.gameState, current_userGame,
-                                  gameDTO.possible_actions(current_userGame.posUserX, current_userGame.posUserY,
-                                                           current_userGame.userNumber), gameDTO.turn)
-            (newPosX, newPosY) = move(current_userGame, moveX, moveY, gameDTO)
-            save_move(current_userGame, newPosX, newPosY, game, gameDTO)
+            current_user_game = user_games[gameDTO.turn - 1]
+            (moveX, moveY) = play(user_game1.posUserX, user_game1.posUserY, user_game2.posUserX, user_game2.posUserY,
+                                  gameDTO.gameState, current_user_game,
+                                  gameDTO.possible_actions(current_user_game.posUserX, current_user_game.posUserY,
+                                                           current_user_game.userNumber), gameDTO.turn)
+            (newPosX, newPosY) = move(current_user_game, moveX, moveY, gameDTO)
+            save_move(current_user_game, newPosX, newPosY, game, gameDTO)
